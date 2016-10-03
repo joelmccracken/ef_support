b35c15ed2b5cd18d3ad73b930e982a5ed43abb10
Joel McCracken <mccracken.joel@gmail.com>
2016-10-03 02:24:17 -0400

    use example elm app to bootstrap

--- web/static/elm/Main.elm
+++ web/static/elm/Main.elm
@@ -1,4 +1,103 @@
-import Html exposing (text)
+import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Json
+import Task
 
+
+type alias Params =
+    { appDataUrl : String}
+
+
+main : Program (Params)
 main =
-  text "Hello, World!"
+  App.programWithFlags
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  {
+  params : Params
+  }
+
+
+init : Params -> (Model, Cmd Msg)
+init params =
+    (Model params, Cmd.none)
+
+
+
+
+-- UPDATE
+
+
+type Msg
+  = MorePlease
+  | FetchSucceed String
+  | FetchFail Http.Error
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (model, Cmd.none)
+      -- (model, getRandomGif model.topic)
+
+    FetchSucceed newUrl ->
+      (model, Cmd.none)
+      -- (Model model.topic newUrl, Cmd.none)
+
+    FetchFail _ ->
+      (model, Cmd.none)
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [] --[text model.topic]
+    , button [ onClick MorePlease ] [ text "More Please!" ]
+    , br [] []
+    , img [] [] --[src model.gifUrl] []
+    ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- HTTP
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in
+    Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
+
+
+decodeGifUrl : Json.Decoder String
+decodeGifUrl =
+  Json.at ["data", "image_url"] Json.string
