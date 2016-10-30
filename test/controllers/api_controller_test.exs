1a02644911fab6f175ffa4723bd3a41f9bc08629
Joel McCracken <mccracken.joel@gmail.com>
2016-10-30 19:53:10 -0400

    test that tasks returned only incl those belonging to user

--- test/controllers/api_controller_test.exs
+++ test/controllers/api_controller_test.exs
@@ -40,7 +40,7 @@ defmodule EFSupport.APIControllerTest do
         |> Map.new()
     end
 
-    test "returns a list of users tasks", %{conn: conn, user: user} do
+    test "returns a list of tasks, only those that belong to user", %{conn: conn, user: user, other_user: other_user} do
       task_attrs = %{
         id: 2,
         name: "a task",
@@ -49,10 +49,22 @@ defmodule EFSupport.APIControllerTest do
       }
 
       struct(Task, task_attrs) |> Repo.insert
+
+      other_task_attrs = %{
+        id: 3,
+        name: "another task",
+        complete: 0,
+        user_id: other_user.id
+      }
+
+      struct(Task, other_task_attrs) |> Repo.insert
+
+      num_tasks = Repo.one(from t in Task, select: count(t.id))
+      assert num_tasks == 2 # two exist, however we should only see one result
+
       conn = get conn, api_path(conn, :app_init)
 
       expected_response_data = %{"tasks" => [convert_map_atom_keys_to_strings task_attrs]}
-
       assert json_response(conn, 200)["data"] == expected_response_data
     end
   end
